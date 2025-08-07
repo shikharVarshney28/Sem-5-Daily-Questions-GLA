fd79531d3af7cdfcd0b8bdd5d2dc9294ae4a84a8
shikharVarshney28 <shikharvarshney1008@gmail.com>
2025-08-07 14:49:00 +0530

    Create 2006-FindTheStudentThatWillReplaceTheChalk

---
+++ 2006-FindTheStudentThatWillReplaceTheChalk/2006-FindTheStudentThatWillReplaceTheChalk.java
@@ -0,0 +1,23 @@
+// Last updated: 8/7/2025, 2:49:00 PM
+class Solution {
+
+        public int chalkReplacer(int[] chalk, int k) {
+                // Find the sum of all elements.
+                        long sum = 0;
+                                for (int i = 0; i < chalk.length; i++) {
+                                            sum += chalk[i];
+                                                        if (sum > k) {
+                                                                        break;
+                                                                                    }
+                                                                                            }
+                                                                                                    // Find modulo of k with sum.
+                                                                                                            k = k % (int) sum;
+                                                                                                                    for (int i = 0; i < chalk.length; i++) {
+                                                                                                                                if (k < chalk[i]) {
+                                                                                                                                                return i;
+                                                                                                                                                            }
+                                                                                                                                                                        k = k - chalk[i];
+                                                                                                                                                                                }
+                                                                                                                                                                                        return 0;
+                                                                                                                                                                                            }
+                                                                                                                                                                                            }
