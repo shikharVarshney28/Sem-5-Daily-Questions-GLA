2e1ff6c3c791d97d0fcfe9caccf9ae0222ea2691
shikharVarshney28 <shikharvarshney1008@gmail.com>
2025-08-07 14:50:52 +0530

    Create 1509-ReplaceEmployeeIdWithTheUniqueIdentifier

---
+++ 1509-ReplaceEmployeeIdWithTheUniqueIdentifier/1509-ReplaceEmployeeIdWithTheUniqueIdentifier.sql
@@ -0,0 +1,4 @@
+-- Last updated: 8/7/2025, 2:50:52 PM
+# Write your MySQL query statement below
+Select unique_id , Employees.name from EmployeeUNI right join Employees on
+EmployeeUNI.id=Employees.id;
