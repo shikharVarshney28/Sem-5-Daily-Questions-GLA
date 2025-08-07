d5f6a6dcbe05a6f476f009992b1af132b7ff9e7d
shikharVarshney28 <shikharvarshney1008@gmail.com>
2025-08-07 14:49:24 +0530

    Create 1878-CheckIfArrayIsSortedAndRotated

---
+++ 1878-CheckIfArrayIsSortedAndRotated/1878-CheckIfArrayIsSortedAndRotated.java
@@ -0,0 +1,26 @@
+// Last updated: 8/7/2025, 2:49:25 PM
+class Solution {
+
+    public boolean check(int[] nums) {
+        int n = nums.length;
+        if (n <= 1)
+            return true;
+
+        int inversionCount = 0;
+
+        // For every pair, count the number of inversions.
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] < nums[i - 1]) {
+                ++inversionCount;
+            }
+        }
+
+        // Also check between the last and the first element due to rotation
+        if (nums[0] < nums[n - 1]) {
+            ++inversionCount;
+        }
+
+        return inversionCount <= 1;
+    }
+
+}
