046d723b7be9fb920c20d0ecc91a8411b494d515
shikharVarshney28 <shikharvarshney1008@gmail.com>
2025-08-07 14:51:26 +0530

    Create 1427-AllElementsInTwoBinarySearchTrees

---
+++ 1427-AllElementsInTwoBinarySearchTrees/1427-AllElementsInTwoBinarySearchTrees.java
@@ -0,0 +1,32 @@
+// Last updated: 8/7/2025, 2:51:26 PM
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public void preorder(TreeNode root,List<Integer> a)
+    {
+        if(root==null) return;
+        a.add(root.val);
+        preorder(root.left,a);
+        preorder(root.right,a);
+    }
+    public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
+        List<Integer> a=new ArrayList<>();
+        preorder(root1,a);
+        preorder(root2,a);
+        Collections.sort(a);
+        return a;
+    }
+}
