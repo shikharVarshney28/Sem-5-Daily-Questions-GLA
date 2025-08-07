a47a27df286ceebc90ede660a336b49d5933e8a4
shikharVarshney28 <shikharvarshney1008@gmail.com>
2025-08-07 14:52:00 +0530

    Create 1245-UserActivityForThePast30DaysI

---
+++ 1245-UserActivityForThePast30DaysI/1245-UserActivityForThePast30DaysI.sql
@@ -0,0 +1,6 @@
+-- Last updated: 8/7/2025, 2:52:01 PM
+# Write your MySQL query statement below
+SELECT  activity_date AS day,COUNT(DISTINCT user_id) AS active_users
+FROM Activity
+WHERE activity_date BETWEEN "2019-06-28" AND "2019-07-27"
+GROUP BY activity_date 
