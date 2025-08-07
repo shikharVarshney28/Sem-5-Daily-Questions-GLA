36723a62634cb44d90848e7719fab95300abf410
shikharVarshney28 <shikharvarshney1008@gmail.com>
2025-08-07 14:47:21 +0530

    Create 2481-StrictlyPalindromicNumber

---
+++ 2481-StrictlyPalindromicNumber/2481-StrictlyPalindromicNumber.java
@@ -0,0 +1,7 @@
+// Last updated: 8/7/2025, 2:47:21 PM
+class Solution {
+    public boolean isStrictlyPalindromic(int n) {
+       // System.out.print(Integer.parseInt(""+n,3));
+        return false;
+    }
+}
