90cf8a57768e0edd686b2be198277596fc1ffc82
Shikhar Varshney <shikharvarshney1008@gmail.com>
2025-10-16 10:44:58 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 10/16/2025, 10:44:58 AM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return ("Hello World")
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
